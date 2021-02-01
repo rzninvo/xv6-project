88859f0fb613cb6c57de21c38e80d3525a5ff574
rzninvo <rzninvo@gmail.com>
2021-02-01 12:20:22 -0800

    edited getparentIDtest

--- getparentIDtest.c
+++ getparentIDtest.c
@@ -4,6 +4,7 @@
 
 int main()
 {
-    printf(1,"this is proccess %d and the parent id is %d\n", getpid(), getparentID());
+    fork();
+    printf(1,"this is proccess %d and the parent id is %d\n", getpid(), getparentID());   
     return 0;
 }
