4817f00560dfd8e288bc66441aa885377bcdc714
arianmotti <arianmotti@github.com>
2021-02-06 15:58:23 -0800

    edited SYSCOUNT

--- param.h
+++ param.h
@@ -12,4 +12,4 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define QUANTUM      2  // quantum time for round-robin scheduling
-#define SYSCOUNT   29
+#define SYSCOUNT   30
